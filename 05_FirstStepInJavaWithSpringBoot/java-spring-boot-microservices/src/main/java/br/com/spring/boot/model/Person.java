c0a466235deaa9e877e22a6676da08b4e5875fac
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-01 20:06:15 -0300

    Feat: added mapping to person entity

--- 05_FirstStepInJavaWithSpringBoot/java-spring-boot-microservices/src/main/java/br/com/spring/boot/model/Person.java
+++ 05_FirstStepInJavaWithSpringBoot/java-spring-boot-microservices/src/main/java/br/com/spring/boot/model/Person.java
@@ -2,20 +2,33 @@ package br.com.spring.boot.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="person")
 public class Person implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name="first_name", nullable = false)
 	private String firstName;
+	
+	@Column(name="last_name")
 	private String lastName;
+	
+	@Column(nullable = false)
 	private String address;
+	
+	@Column(nullable = false)
 	private String gender;
 	
 	public Person() {
