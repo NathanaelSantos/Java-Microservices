112b47752414a47f27cef0aab94913ab948c2b26
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-01 21:32:53 -0300

    Fix: rename classes

--- 05_FirstStepInJavaWithSpringBoot/java-spring-boot-microservices/src/main/java/br/com/spring/boot/exceptions/ResourceNotFoundException.java
+++ 05_FirstStepInJavaWithSpringBoot/java-spring-boot-microservices/src/main/java/br/com/spring/boot/exceptions/ResourceNotFoundException.java
@@ -3,15 +3,12 @@ package br.com.spring.boot.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsuportedMathOperationException extends RuntimeException{
-
-	public UnsuportedMathOperationException(String exception) {
-		super(exception);
-	}
-
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+	
 	private static final long serialVersionUID = 1L;
 	
-	
-
+	public ResourceNotFoundException(String exception) {
+		super(exception);
+	}
 }
--- 05_FirstStepInJavaWithSpringBoot/java-spring-boot-microservices/src/main/java/br/com/spring/boot/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 05_FirstStepInJavaWithSpringBoot/java-spring-boot-microservices/src/main/java/br/com/spring/boot/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,16 +11,16 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.spring.boot.exceptions.ExceptionResponse;
-import br.com.spring.boot.exceptions.UnsuportedMathOperationException;
+import br.com.spring.boot.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
-public class CustomizedResponseEntityException extends ResponseEntityExceptionHandler{
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
 
-	@ExceptionHandler(UnsuportedMathOperationException.class)
-	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception exception, WebRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception exception, WebRequest request) {
 		ExceptionResponse exceptionResptonse = new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
 		
-		return new ResponseEntity<>(exceptionResptonse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResptonse, HttpStatus.NOT_FOUND);
 	}
 }
